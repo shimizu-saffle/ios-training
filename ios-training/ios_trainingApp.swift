19d9735c2c960114eefdafabf032b04f8363c4cc
shimizu-saffle <shimizu.saffle@gmail.com>
2022-12-24 22:15:19 +0900

    Initial Commit

---
+++ ios-training/ios_trainingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_trainingApp.swift
+//  ios-training
+//
+//  Created by Kenta Shimizu on 2022/12/24.
+//
+
+import SwiftUI
+
+@main
+struct ios_trainingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
