2189363f3771067b75e7a5091013774a35a0adba
shimizu-saffle <shimizu.saffle@gmail.com>
2022-12-25 13:27:37 +0900

    feat: good evening

--- ios-training/ContentView.swift
+++ ios-training/ContentView.swift
@@ -10,12 +10,13 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            VStack {
+                Text("Hello, world!")
+                    .padding()
+                Text("good evening ")
+                    .padding()
+            }
         }
-        .padding()
     }
 }
 
